a4b9be439a604fea87a6ff3fff5a288ba85e5651
Jiangxb <Jiangxb@DESKTOP-DOB9M1Q>
2018-09-27 17:07:34 +0800

    增加配置信息

--- imooc-security-demo/src/main/java/com/imooc/security/MyUserDetailsService.java
+++ imooc-security-demo/src/main/java/com/imooc/security/MyUserDetailsService.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
